64fe50df087d3b1a3bb140d1382f6092941a0d24
Aciss? <wawa22arpk@gmail.com>
2026-09-10 16:36:20 +0000

    fs: Implement Dynamic Fsync
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

---
+++ include/linux/dyn_sync_cntrl.h
@@ -0,0 +1,13 @@
+/* 
+ * Dynamic sync control driver definitions (V2)
+ * 
+ * by Aciss? <wawa22arpk@gmail.com>
+ * 
+ */
+
+#define DYN_FSYNC_ACTIVE_DEFAULT false
+#define DYN_FSYNC_VERSION_MAJOR 2
+#define DYN_FSYNC_VERSION_MINOR 2
+
+extern bool suspend_active;
+extern bool dyn_fsync_active;
